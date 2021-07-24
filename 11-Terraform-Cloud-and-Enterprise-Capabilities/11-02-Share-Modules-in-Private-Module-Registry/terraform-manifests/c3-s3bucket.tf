13c18a3a71c3c83d7122a876d808379155106993
DacDo <mr.dacdo@gmail.com>
2021-07-24 19:46:40 +1000

    used dacdo-bucket

--- 11-Terraform-Cloud-and-Enterprise-Capabilities/11-02-Share-Modules-in-Private-Module-Registry/terraform-manifests/c3-s3bucket.tf
+++ 11-Terraform-Cloud-and-Enterprise-Capabilities/11-02-Share-Modules-in-Private-Module-Registry/terraform-manifests/c3-s3bucket.tf
@@ -1,8 +1,8 @@
 # Call our Custom Terraform Module which we built earlier
-module "website_s3_bucket123" {
-  source  = "app.terraform.io/hcta-demo1/s3-website/aws"
-  version = "1.0.1"
+module "website_s3_bucket" {
+  source  = "app.terraform.io/hcta-dacdo/s3-website/aws"
+  version = "1.0.2"
   # insert required variables here
   bucket_name = var.my_s3_bucket
-  tags = var.my_s3_tags
+  tags        = var.my_s3_tags
 }
