5d20a9702b6d009dcdd5f326af4d8b0338610402
DacDo <mr.dacdo@gmail.com>
2021-07-27 05:27:01 +1000

    updated

--- 08-Terraform-Workspaces/v1-local-backend/c5-outputs.tf
+++ 08-Terraform-Workspaces/v1-local-backend/c5-outputs.tf
@@ -3,12 +3,16 @@
 # Attribute Reference
 output "ec2_instance_publicip" {
   description = "EC2 Instance Public IP"
-  value = aws_instance.my-ec2-vm.*.public_ip
+  # value       = aws_instance.my-ec2-vm.*.public_ip
+  value = aws_instance.my-ec2-vm[*].public_ip
 }
 
 
 # Attribute Reference - Create Public DNS URL
 output "ec2_publicdns" {
   description = "Public DNS URL of an EC2 Instance"
-  value = aws_instance.my-ec2-vm.*.public_dns
+  # old syntax
+  # value       = aws_instance.my-ec2-vm.*.public_dns
+  # new syntax
+  value = aws_instance.my-ec2-vm[*].public_dns
 }
