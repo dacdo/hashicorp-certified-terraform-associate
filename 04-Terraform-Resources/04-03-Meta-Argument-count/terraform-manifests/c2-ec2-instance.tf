c99fff94c197cfd0ecb1e23243aab343038ff5b7
DacDo <mr.dacdo@gmail.com>
2021-07-28 20:29:28 +1000

    updated

--- 04-Terraform-Resources/04-03-Meta-Argument-count/terraform-manifests/c2-ec2-instance.tf
+++ 04-Terraform-Resources/04-03-Meta-Argument-count/terraform-manifests/c2-ec2-instance.tf
@@ -1,6 +1,6 @@
 # Create EC2 Instance
 resource "aws_instance" "web" {
-  ami           = "ami-047a51fa27710816e" # Amazon Linux
+  ami           = "ami-083ac7c7ecf9bb9b0" # Amazon Linux
   instance_type = "t2.micro"
   count         = 5
   tags = {
