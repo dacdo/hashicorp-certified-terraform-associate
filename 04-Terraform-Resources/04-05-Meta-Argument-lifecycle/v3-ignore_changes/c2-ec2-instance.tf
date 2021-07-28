056ede6c870156f6adbfa4ccfe1f1b1d05e88d47
DacDo <mr.dacdo@gmail.com>
2021-07-28 22:06:31 +1000

    updated

--- 04-Terraform-Resources/04-05-Meta-Argument-lifecycle/v3-ignore_changes/c2-ec2-instance.tf
+++ 04-Terraform-Resources/04-05-Meta-Argument-lifecycle/v3-ignore_changes/c2-ec2-instance.tf
@@ -1,17 +1,17 @@
 # Create EC2 Instance
 resource "aws_instance" "web" {
-  ami = "ami-0915bcb5fa77e4892" # Amazon Linux
+  ami           = "ami-083ac7c7ecf9bb9b0" # Amazon Linux
   instance_type = "t2.micro"
+  # instance_type = "t2.small"
   tags = {
-    "Name" = "web-3"
+    "Name" = "web-9"
   }
-/*
+
   lifecycle {
     ignore_changes = [
       # Ignore changes to tags, e.g. because a management agent
       # updates these based on some ruleset managed elsewhere.
-      tags,
+      tags, instance_type
     ]
-  }*/
+  }
 }
-
