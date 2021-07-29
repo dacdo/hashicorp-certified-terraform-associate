2261b03a12aac42a411710a1fc963c181cc5179f
DacDo <mr.dacdo@gmail.com>
2021-07-29 10:03:27 +1000

    updated

--- 05-Terraform-Variables/05-01-Terraform-Input-Variables/v9-Input-Variables-Validation-Rules/c2-variables.tf
+++ 05-Terraform-Variables/05-01-Terraform-Input-Variables/v9-Input-Variables-Validation-Rules/c2-variables.tf
@@ -2,15 +2,17 @@
 variable "aws_region" {
   description = "Region in which AWS resources to be created"
   type        = string
-  default     = "us-east-1"
+  default     = "us-west-2"
 }
 
+# To test, export TF_VAR_ec2_ami_id=abc-0915bcb5fa77e4892
+# and run terraform plan
 variable "ec2_ami_id" {
   description = "AMI ID"
   type        = string
   default     = "ami-0915bcb5fa77e4892" # Amazon2 Linux AMI ID
   validation {
-    condition = length(var.ec2_ami_id) > 4 && substr(var.ec2_ami_id, 0, 4) == "ami-"
+    condition     = length(var.ec2_ami_id) > 4 && substr(var.ec2_ami_id, 0, 4) == "ami-"
     error_message = "The ec2_ami_id value must be a valid AMI id, starting with \"ami-\"."
   }
 }
@@ -23,6 +25,6 @@ variable "ec2_instance_count" {
 
 variable "ec2_instance_type" {
   description = "EC2 Instance Type"
-  type = string
-  default = "t3.micro"
+  type        = string
+  default     = "t2.micro"
 }
