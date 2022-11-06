582ed9585e087569b2ff765b3346dec4cfb2d22e
DacDo <mr.dacdo@gmail.com>
2022-11-06 20:15:20 +1100

    update

--- 04-Terraform-Resources/04-05-Meta-Argument-lifecycle/v1-create_before_destroy/c2-ec2-instance.tf
+++ 04-Terraform-Resources/04-05-Meta-Argument-lifecycle/v1-create_before_destroy/c2-ec2-instance.tf
@@ -1,15 +1,14 @@
 # Create EC2 Instance
 resource "aws_instance" "web" {
-  ami               = "ami-0915bcb5fa77e4892" # Amazon Linux
-  instance_type     = "t2.micro"
-  availability_zone = "us-east-1a"
-  #availability_zone = "us-east-1b"
+  # ami               = "ami-0915bcb5fa77e4892" # Amazon Linux
+  ami           = data.aws_ami.amzlinux.id
+  instance_type = "t2.micro"
+  # availability_zone = "us-west-2a"
+  availability_zone = "us-west-2b"
   tags = {
     "Name" = "web-1"
   }
- /*
   lifecycle {
     create_before_destroy = true
   }
-*/
 }
