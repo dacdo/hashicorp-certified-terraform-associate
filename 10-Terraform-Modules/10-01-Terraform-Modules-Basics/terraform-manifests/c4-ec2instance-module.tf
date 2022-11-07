b7098820bf7fde5f33d10419065aeacae771f5fe
DacDo <mr.dacdo@gmail.com>
2022-11-08 09:41:31 +1100

    updating

--- 10-Terraform-Modules/10-01-Terraform-Modules-Basics/terraform-manifests/c4-ec2instance-module.tf
+++ 10-Terraform-Modules/10-01-Terraform-Modules-Basics/terraform-manifests/c4-ec2instance-module.tf
@@ -1,21 +1,20 @@
 # AWS EC2 Instance Module
 module "ec2_cluster" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 2.0"
+  source = "terraform-aws-modules/ec2-instance/aws"
+  # version = "~> 2.0"
+  version = "2.21.0"
 
   name           = "my-modules-demo"
   instance_count = 2
+  count          = 1
 
   ami                    = data.aws_ami.amzlinux.id
   instance_type          = "t2.micro"
   key_name               = "terraform-key"
   monitoring             = true
-  vpc_security_group_ids = ["sg-e64d2cd6"]   # Get Default VPC Security Group ID and replace
-  subnet_id              = "subnet-01ca664b" # Get one public subnet id from default vpc and replace
+  vpc_security_group_ids = ["sg-0dc7ba9db7e970563"]   # Get chef sg id
+  subnet_id              = "subnet-0a9bf9520ede3f0f9" # Get one public subnet id from default vpc and replace
   user_data              = file("apache-install.sh")
-
-  count = 2
-
   tags = {
     Name        = "Modules-Demo"
     Terraform   = "true"
